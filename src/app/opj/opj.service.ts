eb72d859dd0152f415bbdf6c8e45c7f2fd877729
andre-afs <adv.andre.afs@gmail.com>
2017-01-30 21:14:51 -0200

    5 primeiras perguntas

--- src/app/opj/opj.service.ts
+++ src/app/opj/opj.service.ts
@@ -24,9 +24,7 @@ export class OpjService {
         // In a real world app, we might use a remote logging infrastructure
         let errMsg: string;
         if (error instanceof Response) {
-            const body = error.json() || '';
-            const err = body.error || JSON.stringify(body);
-            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+            errMsg = `${error.status} - ${error.statusText || ''}`;
         } else {
             errMsg = error.message ? error.message : error.toString();
         }
